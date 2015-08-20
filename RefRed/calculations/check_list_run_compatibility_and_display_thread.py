004616caa53a0403834063a8fd6f93620953e0ed
Jean Bilheux <bilheuxjm@ornl.gov>
2015-08-20 14:38:30 -0400

    Updating of lconfigdataset is now working

--- RefRed/calculations/check_list_run_compatibility_and_display_thread.py
+++ RefRed/calculations/check_list_run_compatibility_and_display_thread.py
@@ -3,8 +3,13 @@ from PyQt4 import QtCore, QtGui
 
 from RefRed.reduction_table_handling.check_list_run_compatibility import CheckListRunCompatibility
 from RefRed.calculations.add_list_nexus import AddListNexus
+from RefRed.lconfigdataset import LConfigDataset
+
 
 class CheckListRunCompatibilityAndDisplayThread(QtCore.QThread):
+    
+    runs_are_compatible = False
+    wks = None
     
     def setup(self, parent=None,
                  list_run=None,
@@ -20,9 +25,11 @@ class CheckListRunCompatibilityAndDisplayThread(QtCore.QThread):
         self.col = col
         self.is_working_with_data_column = is_working_with_data_column
         self.is_display_requested = is_display_requested
+        self.runs_are_compatible = False
         
     def run(self):
         runs_are_compatible = True
+        wks = []
         if len(self.list_run) > 1:
             o_check_runs = CheckListRunCompatibility(list_nexus = self.list_nexus,
                                                      list_run = self.list_run)
@@ -37,14 +44,35 @@ class CheckListRunCompatibilityAndDisplayThread(QtCore.QThread):
         self.parent.ui.reductionTable.item(self.row, 
                                            self.col).setForeground(_color)
         
-        if not runs_are_compatible:
-            return
-            
-        if self.is_display_requested:
+        if runs_are_compatible:
             o_add_list_nexus = AddListNexus(list_nexus = self.list_nexus,
                                             list_run = self.list_run,
                                             metadata_only = False,
                                             check_nexus_compatibility = False)
             wks = o_add_list_nexus.wks
-            print(wks)
             
+        self.wks = wks
+        self.runs_are_compatible = runs_are_compatible
+        self.update_lconfigdataset()
+            
+    def update_lconfigdataset(self):
+        runs_are_compatible = self.runs_are_compatible
+        big_table_data = self.parent.big_table_data
+        _lconfig = big_table_data[self.row, 2]
+        if _lconfig is None:
+            _lconfig = LConfigDataset()
+        
+        
+        if self.is_working_with_data_column:
+            _lconfig.data_full_file_anme = self.list_nexus
+            _lconfig.data_sets = self.list_run
+            _lconfig.data_wks = self.wks
+            _lconfig.data_runs_compatible = runs_are_compatible
+        else:
+            _lconfig.norm_full_file_anme = self.list_nexus
+            _lconfig.norm_sets = self.list_run
+            _lconfig.norm_wks = self.wks
+            _lconfig.norm_runs_compatible = runs_are_compatible
+            
+        big_table_data[self.row, 2] = _lconfig
+        self.parent.big_table_data = big_table_data
--- RefRed/reduction_table_handling/update_reduction_table.py
+++ RefRed/reduction_table_handling/update_reduction_table.py
@@ -11,6 +11,7 @@ from RefRed.calculations.locate_list_run import LocateListRun
 class UpdateReductionTable(object):
     
     raw_runs = None
+    is_data_displayed = True
     
     def __init__(self, parent=None, row=0, col=1, runs=None):
         self.parent= parent
@@ -23,7 +24,7 @@ class UpdateReductionTable(object):
             return
         
         data_type = 'data' if col == 1 else 'norm'
-        is_data_displayed = True if (col == 1) else False
+        self.is_data_displayed = True if (col == 1) else False
 
         self.raw_runs = str(runs)
         run_breaker = RunSequenceBreaker(run_sequence=self.raw_runs)
@@ -60,99 +61,9 @@ class UpdateReductionTable(object):
                        list_nexus = list_nexus_found,
                        row = row,
                        col = col, 
-                       is_working_with_data_column = is_data_displayed,
+                       is_working_with_data_column = self.is_data_displayed,
                        is_display_requested = self.display_of_this_row_checked())
         self.parent.loading_nxs_thread.start()
-
-        return
-        
-        
-        
-        
-        nxs_loader = CheckListRunCompatibility(list_run=_list_run)
-        #if nxs_loader.no_nexus_found:
-            #_color = QtGui.QColor(RefRed.colors.VALUE_BAD)
-            #self.parent.ui.reductionTable.item(row, 8).setBackground(_color)
-            #self.parent.ui.reductionTable.item(row, 8).setText(data_type + " not found !")
-            #return
-        
-        is_nexus_found = False if nxs_loader.list_nexus_found == None else True
-        if is_nexus_found is False:
-            ClearPlots(self.parent, 
-                       is_data = is_data_displayed,
-                       is_norm = (not is_data_displayed),
-                       plot_yt = True,
-                       plot_yi = True,
-                       plot_it = True,
-                       plot_ix = True)     
-            self.parent.ui.reductionTable.item(row,col).setText('')
-
-            #create empty lconfig
-            big_table_data = self.parent.big_table_data
-            lconfig = big_table_data[row, 2]
-            if lconfig is None:
-                big_table_data[row, 2] = LConfigDataset()
-            else:
-                if is_data_displayed:
-                    lconfig.data_runs_compatible = False
-                else:
-                    lconfig.norm_runs_compatible = False
-            big_table_data[row, 2] = lconfig
-            self.parent.big_table_data = big_table_data
-            
-            return
-
-        self.update_lconfigdataset(nxs_loader)
-
-        #if nxs_loader.runs_compatible:
-            #_color = QtGui.QColor(RefRed.colors.VALUE_OK)
-            #_message = data_type + " runs not compat. !"
-        #else:
-            #_color = QtGui.QColor(RefRed.colors.VALUE_BAD)
-            #_message = ""
-        #self.parent.ui.reductionTable.item(row, 8).setBackground(_color)
-        ##self.parent.ui.reductionTable.item(row, message_col_index).setText(_message)
-    
-        if self.display_of_this_row_checked():
-            DisplayReductionTable(parent=self.parent, 
-                                  row=self.row,
-                                  is_data_displayed=is_data_displayed)
-        else:
-            ClearPlots(self.parent, 
-                       is_data = is_data_displayed,
-                       is_norm = (not is_data_displayed),
-                       plot_yt = True,
-                       plot_yi = True,
-                       plot_it = True,
-                       plot_ix = True)
-    
-    def update_lconfigdataset(self, nxs_loader):
-        list_nexus_found = nxs_loader.list_nexus_found
-        list_run_found = nxs_loader.list_run_found
-        list_wks = nxs_loader.list_wks
-        runs_compatible = nxs_loader.runs_compatible
-        
-        _row = self.row
-        big_table_data = self.parent.big_table_data
-        
-        if big_table_data[_row, 2] is None:
-            _lconfig = LConfigDataset()
-        else:
-            _lconfig = big_table_data[_row, 2]
-            
-        if self.col == 1: #data
-            _lconfig.data_full_file_name = list_nexus_found
-            _lconfig.data_sets = list_run_found
-            _lconfig.data_wks =  list_wks
-            _lconfig.data_runs_compatible = runs_compatible
-        else: #norm
-            _lconfig.norm_full_file_name = list_nexus_found
-            _lconfig.norm_sets = list_run_found
-            _lconfig.norm_wks = list_wks
-            _lconfig.norm_runs_compatible = runs_compatible
-            
-        big_table_data[_row, 2] = _lconfig
-        self.parent.big_table_data = big_table_data
 
     def clear_cell(self):
         print('in clear cell')
