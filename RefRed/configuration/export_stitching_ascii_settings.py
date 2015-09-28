fde7cf240106e2403b5752cbe7c7b83649686c13
Jean Bilheux <bilheuxjm@ornl.gov>
2015-09-28 09:45:49 -0400

    Change default output ascii settings for reduced data

--- RefRed/configuration/export_stitching_ascii_settings.py
+++ RefRed/configuration/export_stitching_ascii_settings.py
@@ -1,8 +1,8 @@
 class ExportStitchingAsciiSettings(object):
 	
 	fourth_column_flag = True
-	fourth_column_dq0 = 0.05
-	fourth_column_dq_over_q = 0.001
+	fourth_column_dq0 = 0.0004
+	fourth_column_dq_over_q = 0.023
 	use_lowest_error_value_flag = True
 	
 	
