c534f867f40ea47b38f8664ce715a68d9bbc45d5
Jean Bilheux <bilheuxjm@ornl.gov>
2015-10-22 15:56:11 -0400

    Changed default angle offset value

--- RefRed/interfaces/display_metadata_interface.py
+++ RefRed/interfaces/display_metadata_interface.py
@@ -3,7 +3,7 @@
 
 # Form implementation generated from reading ui file 'designer//display_metadata_interface.ui'
 #
-# Created: Thu Oct 22 13:36:15 2015
+# Created: Thu Oct 22 15:55:43 2015
 #      by: PyQt4 UI code generator 4.7.4
 #
 # WARNING! All changes made in this file will be lost!
--- RefRed/interfaces/plot2d_dialog_refl_interface.py
+++ RefRed/interfaces/plot2d_dialog_refl_interface.py
@@ -3,7 +3,7 @@
 
 # Form implementation generated from reading ui file 'designer//plot2d_dialog_refl_interface.ui'
 #
-# Created: Thu Oct 22 13:36:15 2015
+# Created: Thu Oct 22 15:55:44 2015
 #      by: PyQt4 UI code generator 4.7.4
 #
 # WARNING! All changes made in this file will be lost!
