9eb7b28a4acc643fc9678a63c109d5aa4577e591
Jean Bilheux <bilheuxjm@ornl.gov>
2015-10-28 12:32:26 -0400

    manual sf starts now with the same value as the clocking value

--- RefRed/reduction/live_reduced_data_handler.py
+++ RefRed/reduction/live_reduced_data_handler.py
@@ -57,6 +57,7 @@ class LiveReducedDataHandler(object):
         #manual SF
         _widget_manual = QtGui.QDoubleSpinBox()
         _widget_manual.setMinimum(0)
+        sf_manual = _lconfig.sf_clocking
         _widget_manual.setValue(_lconfig.sf_manual)
         _widget_manual.setSingleStep(0.01)
         _widget_manual.valueChanged.connect(self.parent.data_stitching_table_manual_spin_box)
