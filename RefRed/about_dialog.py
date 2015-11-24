b9fa196bcccd0c2f45ffd9b1aa2aec6935903dfb
Jean Bilheux <bilheuxjm@ornl.gov>
2015-11-24 14:39:43 -0500

    Finished up implementation of about dialog message

--- RefRed/about_dialog.py
+++ RefRed/about_dialog.py
@@ -1,9 +1,10 @@
-from numpy.version import version as numpy_version
-from matplotlib import __version__ as matplotlib_version
+from numpy.version import version as numpy_version_str
+from matplotlib import __version__ as matplotlib_version_str
 from PyQt4.pyqtconfig import Configuration as pyqt_configuration
 import RefRed.version
 from PyQt4 import QtGui, QtCore
 import sys
+import mantid
 
 
 class AboutDialog(object):
@@ -15,29 +16,38 @@ class AboutDialog(object):
 
         def display(self):
                 RefRed_version = RefRed.version.str_version
-                python_version =  sys.version
-                numpy_version = numpy_version
-                mantid_version = ''
-                matplotlib_version = matplotlib_version
+                python_version =  self.get_python_version()
+                numpy_version = numpy_version_str
+                mantid_version = mantid.__version__
+                matplotlib_version = matplotlib_version_str
                 qt_version = QtCore.QT_VERSION_STR
                 pyqt_version = pyqt_configuration().pyqt_version_str
 
                 message = '''RefRed - Liquids Reflectrometry Reduction program
-                version %s on Python %s
+
+                version %s 
                 
                 Library versions:
-                  - Numpy %s
-                  - Mantid  %s
-                  - Matplotlib %s
-                  - Qt %s
-                  - PyQt %s''' %(ref_red_version, 
+                  - Python: %s
+                  - Numpy: %s
+                  - Mantid:  %s
+                  - Matplotlib: %s
+                  - Qt: %s
+                  - PyQt: %s''' %(RefRed_version, 
                                  python_version,
                                  numpy_version,
                                  mantid_version,
                                  matplotlib_version,
                                  qt_version,
                                  pyqt_version)
+                
+                QtGui.QMessageBox.about(self.parent, 'About RefRed', message)
 
-                QtGui.QMessageBox.about(self, 'About RefRed', message)
 
-
+        def get_python_version(self):
+                str_version = sys.version_info
+                str_array = []
+                for value in str_version:
+                        str_array.append(str(value))
+                return ".".join(str_array[0:3])
+                
