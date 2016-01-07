00e4428c84f83cda560a9b8f0f82df7568febd44
Jean Bilheux <bilheuxjm@ornl.gov>
2016-01-07 16:36:01 -0500

    Fixed wrong call and started to work on Absolute Normalization

--- RefRed/reduction/normalization_stitching.py
+++ RefRed/reduction/normalization_stitching.py
@@ -1,4 +1,3 @@
-from RefRed.utilities import weighted_mean_default
 from RefRed.reduction.calculate_sf_overlap_range import CalculateSFoverlapRange
 from RefRed.reduction.calculate_sf_critical_edge import CalculateSFCE
 
@@ -8,6 +7,20 @@ class ParentHandler(object):
     def __init__(self, parent=None, row_index=0):
         self.parent = parent
         self.row_index = row_index
+
+    def calculateSFCE(self):
+        '''
+        Scaling factor calculation of Ctritical Edge (CE)
+        '''
+        _q_min = float(str(self.parent.ui.sf_qmin_value.text()))
+        _q_max = float(str(self.parent.ui.sf_qmax_value.text()))
+        big_table_data = self.parent.big_table_data
+        data_set = big_table_data[0, 2]
+        calculate_sf = CalculateSFCE([_q_min, _q_max], data_set)
+        _sf = 1./calculate_sf.getSF()
+        data_set.sf_auto = _sf
+        big_table_data[0, 2] = data_set
+        self.parent.big_table_data = big_table_data
 
 class AbsoluteNormalization(ParentHandler):
     '''
@@ -20,12 +33,41 @@ class AbsoluteNormalization(ParentHandler):
         
     def run(self):
         if self.parent.ui.sf_button.isChecked():
-            self.use_manually_defined_sf()
+            self.useManuallyDefineSF()
         else:
-            self.use_first_angle_range()
+            self.useFirstAngleRange()
     
-    def use_manually_defined_sf(self):
-        manual_sf = float(str(self.parent.ui.sf_value.text()))
+    def useManuallyDefineSF(self):
+        _sf = float(str(self.parent.ui.sf_value.text()))
+        big_table_data = self.parent.big_table_data
+        data_set = big_table_data[0, 2]
+        data_set.sf_abs_normalization = _sf
+        big_table_data[0, 2] = data_set
+        self.parent.big_table_data = big_table_data
+        
+    def useFirstAngleRange(self):
+        self.calculateSFCE(self)
+        
+        
+        
+        
+        
+        
+        
+        
+
+
+class AutomaticStitching(ParentHandler):
+    '''
+    automatic stiching of the reduced data using the Q range to calculate the CE
+    '''
+    
+    def __init__(self, parent=None, row_index=0):
+        super(AutomaticStitching, self).__init__(parent = parent,
+                                                 row_index = row_index)
+        
+    def run(self):
+        self.use_first_angle_range()
     
     def use_first_angle_range(self):
         if self.row_index == 0:
@@ -33,19 +75,6 @@ class AbsoluteNormalization(ParentHandler):
         else:
             self.calcualteSFOtherAngles(self)
     
-    def calculateSFCE(self):
-        '''
-        Scaling factor calculation of Ctritical Edge (CE)
-        '''
-        _q_min = float(str(self.parent.ui.sf_qmin_value.text()))
-        _q_max = float(str(self.parent.ui.sf_qmax_value.text()))
-        big_table_data = self.parent.big_table_data
-        data_set = big_table_data[0, 2]
-        calculate_sf = CalculateSFCE([_q_min, _q_max], data_set)
-        _sf = 1./calculate_sf.run()
-        data_set.sf_auto = _sf
-        big_table_data[0, 2] = data_set
-        self.parent.big_table_data = big_table_data
     
     def calculateSFOtherAngles(self):
         '''
@@ -60,19 +89,6 @@ class AbsoluteNormalization(ParentHandler):
         right_lconfig.sf_auto = _sf
         self.big_table_data[_row_index, 2] = right_lconfig
         self.parent.big_table_data = big_table_data
-
-
-class AutomaticStitching(ParentHandler):
-    '''
-    automatic stiching of the reduced data using the Q range to calculate the CE
-    '''
-    
-    def __init__(self, parent=None, row_index=0):
-        super(AutomaticStitching, self).__init__(parent = parent,
-                                                 row_index = row_index)
-        
-    def run(self):
-        pass
     
     
 class ManualStitching(ParentHandler):
