b184d7031e8031c7d652db958deeade4fdbea2fe
Jean Bilheux <bilheuxjm@ornl.gov>
2016-02-24 15:31:13 -0500

    Ready to build rpm

--- RefRed/interfaces/icons_rc.py
+++ RefRed/interfaces/icons_rc.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created by: The Resource Compiler for PyQt (Qt v4.6.2)
+# Created by: The Resource Compiler for PyQt (Qt v4.8.5)
 #
 # WARNING! All changes made in this file will be lost!
 
