fd7ab6da672ea4f7b8894fb7554d2d11a4a2d9e9
Jean Bilheux <bilheuxjm@ornl.gov>
2016-03-04 17:16:00 -0500

    changed version

--- RefRed/version.py
+++ RefRed/version.py
@@ -3,6 +3,6 @@
 # the minor version and change date gets automatically updated on a git commit
 window_title = 'Liquids Reflectometer Reduction - '
 
-version = (2, 33)
+version = (2, 34)
 last_changes = ""
 str_version = ".".join(map(str, version))
