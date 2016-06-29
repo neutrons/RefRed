53fc974db87c1eb2a6c75f817841aa1ec6ced55c
Jean Bilheux <bilheuxjm@ornl.gov>
2016-06-29 16:11:59 -0400

    Fixed SAVE button and increased list of colors

--- RefRed/configuration/saving_configuration.py
+++ RefRed/configuration/saving_configuration.py
@@ -30,18 +30,18 @@ class SavingConfiguration(object):
             if self.filename == '':
                 return
 
-            self.parent.path_config = os.path.dirname(self.filename)
-            self.filename = makeSureFileHasExtension(self.filename)
-            o_export = ExportXMLConfig(parent = self.parent,
-                                       filename = self.filename)
-            
-            StatusMessageHandler(parent = self.parent, 
-                                 message = 'Done!', 
-                                 is_threaded = True)
-            
-            o_gui_utility = GuiUtility(parent = self.parent)
-            o_gui_utility.new_config_file_loaded(config_file_name = self.filename)
-            o_gui_utility.gui_not_modified()
+        self.parent.path_config = os.path.dirname(self.filename)
+        self.filename = makeSureFileHasExtension(self.filename)
+        o_export = ExportXMLConfig(parent = self.parent,
+                                   filename = self.filename)
+        
+        StatusMessageHandler(parent = self.parent, 
+                             message = 'Done!', 
+                             is_threaded = True)
+        
+        o_gui_utility = GuiUtility(parent = self.parent)
+        o_gui_utility.new_config_file_loaded(config_file_name = self.filename)
+        o_gui_utility.gui_not_modified()
 
     
             
