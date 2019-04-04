0f5fcb4e4ca0ea362b26e4367941f55420b19b1a
Mathieu Doucet <doucetm@ornl.gov>
2019-04-04 13:45:28 -0400

    flake8

--- RefRed/gui_handling/first_angle_range_gui_handler.py
+++ RefRed/gui_handling/first_angle_range_gui_handler.py
@@ -13,8 +13,8 @@ class FirstAngleRangeGuiHandler(ParentGuiHandler):
 
     def __init__(self, parent=None):
         super(FirstAngleRangeGuiHandler, self).__init__(parent=parent)
-        
-    def setWidgets(self, is_sf_button_clicked = True):
+
+    def setWidgets(self, is_sf_button_clicked=True):
         self.parent.ui.qmin_label.setEnabled(not is_sf_button_clicked)
         self.parent.ui.qmax_label.setEnabled(not is_sf_button_clicked)
         self.parent.ui.sf_qmin_value.setEnabled(not is_sf_button_clicked)
@@ -25,16 +25,16 @@ class FirstAngleRangeGuiHandler(ParentGuiHandler):
         
     def is_sf_button_clicked(self):
         return self.parent.ui.sf_button.isChecked()
-    
+
 
 class NormalizationOrStitchingButtonStatus(ParentGuiHandler):
-    
+
     is_absolute_normalization = True
     is_auto_stitching = False
     is_manual_stitching = False
-    
+
     def __init__(self, parent=None):
-        super(NormalizationOrStitchingButtonStatus, self).__init__(parent = parent)
+        super(NormalizationOrStitchingButtonStatus, self).__init__(parent=parent)
 
     def setWidget(self, activated_button=0):
         self.resetAllWidgets()
@@ -44,30 +44,30 @@ class NormalizationOrStitchingButtonStatus(ParentGuiHandler):
             self.is_auto_stitching = True
         else:
             self.is_manual_stitching = True
-            
+
         self.handleWidgets()
-        
+
     def handleWidgets(self):
         if self.is_manual_stitching:
             self.parent.ui.sf_first_angle_range_group.setEnabled(False)
             return
         else:
             self.parent.ui.sf_first_angle_range_group.setEnabled(True)
-            
-        first_angle_handler = FirstAngleRangeGuiHandler(parent = self.parent)
+
+        first_angle_handler = FirstAngleRangeGuiHandler(parent=self.parent)
         is_sf_button_clicked = first_angle_handler.is_sf_button_clicked()
         if self.is_auto_stitching:
             self.parent.ui.first_angle_range_button.setEnabled(False)
-            first_angle_handler.setWidgets(is_sf_button_clicked = False)
+            first_angle_handler.setWidgets(is_sf_button_clicked=False)
             self.parent.ui.sf_button.setEnabled(False)
             self.parent.ui.sf_value.setEnabled(False)
             return
         else:
             self.parent.ui.first_angle_range_button.setEnabled(True)
-            first_angle_handler.setWidgets(is_sf_button_clicked = is_sf_button_clicked)
+            first_angle_handler.setWidgets(is_sf_button_clicked=is_sf_button_clicked)
             self.parent.ui.sf_button.setEnabled(True)
             self.parent.ui.sf_value.setEnabled(True)
-        
+
     def resetAllWidgets(self):
         self.is_absolute_normalization = False
         self.is_auto_stitching = False
@@ -80,9 +80,5 @@ class NormalizationOrStitchingGuiHandler(NormalizationOrStitchingButtonStatus):
     status of the 3 main buttons: Absolute_Normalization, Auto._Stitching and
     Manual_Stitching
     '''
-    
     def __init__(self, parent=None):
-        super(NormalizationOrStitchingGuiHandler, self).__init__(parent = parent)
-        
-    
-        
+        super(NormalizationOrStitchingGuiHandler, self).__init__(parent=parent)
