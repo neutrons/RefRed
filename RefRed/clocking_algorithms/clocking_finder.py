bdcd4baf8f7a619eea7d9219c001b9b6b65b08ff
Mathieu Doucet <doucetm@ornl.gov>
2019-04-04 16:07:08 -0400

    flake8

--- RefRed/clocking_algorithms/clocking_finder.py
+++ RefRed/clocking_algorithms/clocking_finder.py
@@ -1,8 +1,9 @@
 import numpy as np
 import math
 
+
 class ClockingAlgorithm(object):
-    
+
     clocking_left = -1
     clocking_right = -1
 
@@ -16,20 +17,19 @@ class ClockingAlgorithm(object):
     deri_max_pixel_value = -1
     mean_counts_firstderi = -1
     std_deviation_counts_firstderi = -1
-    
+
     def __init__(self, xdata=None, ydata=None, edata=None):
         self.initArrays()
 
         self.xdata = np.array(xdata)
         self.ydata = np.array(ydata)
         self.edata = np.array(edata)
-        
-        self.calculate_first_derivative() #step1
-        self.calculate_min_max_derivative_pixels() #step2
-        self.calculate_avg_and_std_derivative() #step3
 
-        self.calculate_main_central_peak_region() #step4
-        
+        self.calculate_first_derivative()
+        self.calculate_min_max_derivative_pixels()
+        self.calculate_avg_and_std_derivative()
+        self.calculate_main_central_peak_region()
+
     def initArrays(self):
         self.xdata_firstderi = []
         self.ydata_firstderi = []
@@ -45,33 +45,33 @@ class ClockingAlgorithm(object):
     def calculate_first_derivative(self):
         xdata = self.xdata
         ydata = self.ydata
-        
+
         _xdata_firstderi = []
         _ydata_firstderi = []
         for i in range(len(xdata)-1):
             _left_x = xdata[i]
             _right_x = xdata[i+1]
             _xdata_firstderi.append(np.mean([_left_x, _right_x]))
-            
+
             _left_y = ydata[i]
             _right_y = ydata[i+1]
             _ydata_firstderi.append((_right_y - _left_y)/(_right_x - _left_x))
-        
+
         self.xdata_firstderi = _xdata_firstderi
         self.ydata_firstderi = _ydata_firstderi
-        
+
     def calculate_min_max_derivative_pixels(self):
         _pixel = self.xdata_firstderi
         _counts_firstderi = self.ydata_firstderi
-        
+
         _sort_counts_firstderi = np.sort(_counts_firstderi)
         self.deri_min = _sort_counts_firstderi[0]
         self.deri_max = _sort_counts_firstderi[-1]
-        
+
         _sort_index = np.argsort(_counts_firstderi)
         self.deri_min_pixel_value = min([_pixel[_sort_index[0]], _pixel[_sort_index[-1]]])
         self.deri_max_pixel_value = max([_pixel[_sort_index[0]], _pixel[_sort_index[-1]]])
-        
+
     def calculate_avg_and_std_derivative(self):
         _counts_firstderi = np.array(self.ydata_firstderi)
         self.mean_counts_firstderi = np.mean(_counts_firstderi)
@@ -79,10 +79,9 @@ class ClockingAlgorithm(object):
         self.std_deviation_counts_firstderi = math.sqrt(_mean_counts_firstderi)
 
     def calculate_main_central_peak_region(self):
-        
         _counts = self.ydata_firstderi
         _pixel = self.xdata_firstderi
-        
+
         _std_deviation_counts_firstderi = self.std_deviation_counts_firstderi
 
         # starting from left
@@ -91,20 +90,19 @@ class ClockingAlgorithm(object):
             px_offset += 1
         _tmp_value = _pixel[int(px_offset)]
         _peak_min_final_value = _tmp_value if _tmp_value > 5 else 5
-            
+
         px_offset = len(_counts)-1
         while abs(_counts[int(round(px_offset))]) < _std_deviation_counts_firstderi:
             px_offset -= 1
         _tmp_value = _pixel[int(round(px_offset))]
         _peak_max_final_value = _tmp_value if _tmp_value < 250 else 250
-        
-        
-        self.clocking = [int(_peak_min_final_value), int(np.ceil(_peak_max_final_value))]
-    
-class ClockingFinder(object):
 
+        self.clocking = [int(_peak_min_final_value), int(np.ceil(_peak_max_final_value))]
+
+
+class ClockingFinder(object):
     clocking = [-1, -1]
-    
+
     def __init__(self, parent=None, xdata=None, ydata=None, edata=None):
         self.clocking = [-1, -1]
         self.parent = parent
@@ -112,23 +110,22 @@ class ClockingFinder(object):
         # of a better peak-finding algorithm
         self.clocking = self.parent.gui_metadata['clocking_pixel']
         return
-        
+
         o_clocking_algo = ClockingAlgorithm(xdata = xdata,
                                             ydata = ydata,
                                             edata = edata)
         [left_max, right_min] = o_clocking_algo.clocking
-        
+
         left_xdata = xdata[0: left_max]
         left_ydata = ydata[0: left_max]
         o_clocking_left = ClockingAlgorithm(xdata = left_xdata,
                                             ydata = left_ydata)
         left_clocking = o_clocking_algo.clocking[0]
-        
+
         right_xdata = xdata[right_min: -1]
         right_ydata = ydata[right_min: -1]
         o_clocking_right = ClockingAlgorithm(xdata = right_xdata,
                                              ydata = right_ydata)
         right_clocking = o_clocking_right.clocking[1]
-        
+
         self.clocking = [left_clocking, right_clocking]
-        
--- RefRed/gui_handling/update_plot_widget_status.py
+++ RefRed/gui_handling/update_plot_widget_status.py
@@ -5,32 +5,32 @@ class UpdatePlotWidgetStatus(object):
     '''
     This class will check if the plot widgets can be enabled according to
     status of the main table and tab selected
-    '''    
+    '''
     parent = None
-    
+
     def __init__(self, parent=None):
         self.parent = parent
 
     def disable_all(self):
-        self.data_tab_widgets(status = False)
-        self.norm_tab_widgets(status = False)
-        
+        self.data_tab_widgets(status=False)
+        self.norm_tab_widgets(status=False)
+
     def disable_data(self):
-        self.data_tab_widgets(status = False)
-        self.data_clocking_widgets(status = False)
+        self.data_tab_widgets(status=False)
+        self.data_clocking_widgets(status=False)
 
     def enable_data(self):
-        self.data_tab_widgets(status = True)
-        self.data_clocking_widgets(status = True)        
-        
+        self.data_tab_widgets(status=True)
+        self.data_clocking_widgets(status=True)
+
     def disable_norm(self):
-        self.norm_tab_widgets(status = False)
+        self.norm_tab_widgets(status=False)
 
     def enable_norm(self):
-        self.norm_tab_widgets(status = True)
-        
-    def data_clocking_widgets(self, status = False):
-        o_gui_utility = GuiUtility(parent = self.parent)
+        self.norm_tab_widgets(status=True)
+
+    def data_clocking_widgets(self, status=False):
+        o_gui_utility = GuiUtility(parent=self.parent)
         if o_gui_utility.is_row_with_highest_q():
             status_clocking = True
         else:
@@ -43,22 +43,19 @@ class UpdatePlotWidgetStatus(object):
             else:
                 status_enable = True
         else:
-            status_visible = True #always visible
+            status_visible = True # always visible
             status_enable = False
 
         self.parent.ui.dataPrimFromLabel.setVisible(status_visible)
         self.parent.ui.dataPrimFromLabel.setEnabled(status_enable)
         self.parent.ui.dataPrimFromValue.setVisible(status_visible)
         self.parent.ui.dataPrimFromValue.setEnabled(status_enable)
-        
+
         self.parent.ui.dataPrimToLabel.setVisible(status_visible)
         self.parent.ui.dataPrimToLabel.setEnabled(status_enable)
         self.parent.ui.dataPrimToValue.setVisible(status_visible)
         self.parent.ui.dataPrimToValue.setEnabled(status_enable)
 
-    def disable_reduced(self):
-        parent = self.parent
-        
     def data_tab_widgets(self, status=False):
         parent = self.parent
         parent.ui.dataNormTabWidget.setEnabled(status)
@@ -81,13 +78,12 @@ class UpdatePlotWidgetStatus(object):
         self.parent.ui.dataTOFmanualLabel.setEnabled(status)
         self.parent.ui.dataTOFautoMode.setEnabled(status)
         self.parent.ui.dataTOFmanualMode.setEnabled(status)
-        o_gui_utility = GuiUtility(parent = self.parent)
+        o_gui_utility = GuiUtility(parent=self.parent)
         if status:
             is_auto_tof_selected = o_gui_utility.is_auto_tof_range_radio_button_selected()
-            o_gui_utility.set_auto_tof_range_widgets(status = is_auto_tof_selected)
+            o_gui_utility.set_auto_tof_range_widgets(status=is_auto_tof_selected)
         else:
-            o_gui_utility.set_auto_tof_range_widgets(status = True)
-            
+            o_gui_utility.set_auto_tof_range_widgets(status=True)
 
     def norm_tab_widgets(self, status=False):
         parent = self.parent
@@ -107,15 +103,14 @@ class UpdatePlotWidgetStatus(object):
         parent.ui.normPeakToValue.setEnabled(status)
         parent.ui.normPeakFromValue.setEnabled(status)
         parent.ui.normBackgroundFlag.setEnabled(status)
-            
+
         self.parent.ui.dataTOFmanualLabel.setEnabled(status)
         self.parent.ui.dataTOFautoMode.setEnabled(status)
         self.parent.ui.dataTOFmanualMode.setEnabled(status)
 
-        o_gui_utility = GuiUtility(parent = self.parent)
+        o_gui_utility = GuiUtility(parent=self.parent)
         if status:
             is_auto_tof_selected = o_gui_utility.is_auto_tof_range_radio_button_selected()
-            o_gui_utility.set_auto_tof_range_widgets(status = is_auto_tof_selected)
+            o_gui_utility.set_auto_tof_range_widgets(status=is_auto_tof_selected)
         else:
-            o_gui_utility.set_auto_tof_range_widgets(status = True)
-            
+            o_gui_utility.set_auto_tof_range_widgets(status=True)
