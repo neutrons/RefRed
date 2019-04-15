359cae16a8e8475c5ab2e0c0aabac20ba2b9a1d7
Mathieu Doucet <doucetm@ornl.gov>
2019-04-15 09:19:59 -0400

    remove dead code

--- RefRed/thread/locate_run.py
+++ RefRed/thread/locate_run.py
@@ -1,17 +1,14 @@
-from PyQt4 import QtCore, QtGui
-import time
-import mantid
-import sys
-from mantid.simpleapi import *
+from PyQt4 import QtCore
 import RefRed.nexus_utilities
+
 
 class LocateRunThread(QtCore.QThread):
 
-    def setup(self, parent, run_number, index):   
+    def setup(self, parent, run_number, index):
         self.parent = parent
         self.run_number = run_number
         self.index = index
-    
+
     def run(self):
         try:
             full_file_name = RefRed.nexus_utilities.findNeXusFullPath(self.run_number)
@@ -24,25 +21,11 @@ class LocateRunThread(QtCore.QThread):
         else:
             self.parent.list_nxs[self.index] = full_file_name
             self.parent.runs_found += 1
-            
-    def stop(self):
-        pass
-        
-    def pause(self):
-        pass
-        
 
-class LoadRunThread(QtCore.QThread):
-    
-    def setup(self, parent, file_name, output_wks, index):
-        self.parent = parent
-        self.file_name = file_name
-        self.output_wks = output_wks
-        self.index = index
-        
-    def run(self):
-        _workspace = LoadEventNexus(Filename = self.file_name,
-                                    OutputWorkspace = self.output_wks,
-                                    MetadataOnly = True)
-        self.parent.list_wks[self.index] = self.output_wks
-        self.parent.runs_loaded += 1
+    def stop(self):
+        # TODO: investigate why this was implemented this way and remove this method
+        pass
+
+    def pause(self):
+        # TODO: investigate why this was implemented this way and remove this method
+        pass
