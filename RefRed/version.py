7774bd5ee98a67b93b73e75a054a00b44c08d6da
Donnie Earnest <earnestdl@ornl.gov>
2021-04-16 12:40:30 -0500

    update version

--- RefRed/version.py
+++ RefRed/version.py
@@ -3,7 +3,7 @@
 # the minor version and change date gets automatically updated on a git commit
 window_title = 'Liquids Reflectometer Reduction - '
 
-version = (2, 55)
+version = (2, 57)
 
 last_changes = ""
 str_version = ".".join(map(str, version))
