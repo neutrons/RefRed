255479cceeee2947e2ef6841e72465a825dc1286
Michael Walsh <walshmm@ornl.gov>
2021-09-16 09:22:31 -0400

    forgot to change the env variable

--- .github/workflows/install.sh
+++ .github/workflows/install.sh
@@ -1,7 +1,7 @@
 #- sudo apt-get update
 # We do this conditionally because it saves us some downloading if the
 # version is the same.
-if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
+if [[ "$PYTHON_VERSION" == "2.7" ]]; then
   wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh;
 else
   wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
@@ -15,7 +15,7 @@ conda update -q conda
 # Useful for debugging any issues with conda
 conda info -a
 # Replace dep1 dep2 ... with your dependencies
-conda create -q -n test-environment python=$TRAVIS_PYTHON_VERSION numpy pyqt=4;
+conda create -q -n test-environment python=$PYTHON_VERSION numpy pyqt=4;
 source activate test-environment
 conda config --add channels conda-forge
 #- conda config --add channels mantid
