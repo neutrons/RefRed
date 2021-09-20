82578b4089b5e9921f058eb8116243deb817b133
Michael Walsh <walshmm@ornl.gov>
2021-09-20 12:48:35 -0400

    change to match other conda env init

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -1,4 +1,4 @@
 #- python setup.py install
-conda activate test-environment
+source activate test-environment
 flake8 --exit-zero --statistics --config build_tools/flake8.cfg RefRed
 cd test;coverage run peak_finder_derivation_test.py
