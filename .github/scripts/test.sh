a08a8ed04840f6b09f260c613a87a1de28486283
Michael Walsh <walshmm@ornl.gov>
2021-09-20 13:18:35 -0400

    more fiddling with conda

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -1,4 +1,4 @@
 #- python setup.py install
-activate test-environment
-flake8 --exit-zero --statistics --config build_tools/flake8.cfg RefRed
-cd test;coverage run peak_finder_derivation_test.py
+conda activate test-environment
+conda flake8 --exit-zero --statistics --config build_tools/flake8.cfg RefRed
+cd test;conda coverage run peak_finder_derivation_test.py
