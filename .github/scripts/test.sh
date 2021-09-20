b05909386d2f7436189e54d8b18a12e29da565ee
Michael Walsh <walshmm@ornl.gov>
2021-09-20 14:55:41 -0400

    try using setup.py instead of manual install

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -7,6 +7,6 @@ conda config --add channels conda-forge
 #- conda install --yes -c mantid/label/nightly mantid-framework
 pip install --upgrade codecov
 pip install flake8
-pip install numpy
+python setup.py install
 flake8 --exit-zero --statistics --config build_tools/flake8.cfg RefRed
 cd test;coverage run peak_finder_derivation_test.py
