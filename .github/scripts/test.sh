657b6d7d36a33924a4602309b60b2674598692dd
Michael Walsh <walshmm@ornl.gov>
2021-09-20 12:23:19 -0400

    add python command, flake not on path

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -1,3 +1,3 @@
 #- python setup.py install
-flake8 --exit-zero --statistics --config build_tools/flake8.cfg RefRed
-cd test; coverage run peak_finder_derivation_test.py
+python -m flake8 --exit-zero --statistics --config build_tools/flake8.cfg RefRed
+cd test;python -m coverage run peak_finder_derivation_test.py
