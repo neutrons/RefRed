cab224095e5cd60fca397a38a3367773fb049b8f
Michael Walsh <walshmm@ornl.gov>
2021-09-23 09:49:04 -0400

    utilize requirements txt files

--- .github/scripts/install.sh
+++ .github/scripts/install.sh
@@ -3,7 +3,3 @@ conda info -a
 
 conda create -q -n test-environment python=$PYTHON_VERSION numpy pyqt=4;
 source activate test-environment
-
-pip install --upgrade codecov
-pip install flake8
-pip install numpy
--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -1,2 +1,2 @@
 flake8 --exit-zero --statistics --config build_tools/flake8.cfg RefRed
-cd test;coverage run peak_finder_derivation_test.py
+pytest --cov=RefRed test/
