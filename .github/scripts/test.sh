a8739b9059f31da9f2bf0fb478d21bfe3f893c2e
Ross Whitfield <whitfieldre@ornl.gov>
2021-09-28 14:08:16 -0400

    Fix the last two failing tests

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -1,3 +1,2 @@
 flake8 --exit-zero --statistics RefRed
-# allow tests to pass until they are all fixed
-pytest --cov=RefRed --cov-report=xml --cov-report=term test || true
+python -m pytest --cov=RefRed --cov-report=xml --cov-report=term test
