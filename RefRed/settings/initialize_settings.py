f46a39f7aa245713a72414a6561895e65e7f619f
Michael Walsh <walshmm@ornl.gov>
2021-10-19 13:15:56 -0400

    cleanup initialize_settings.py

--- RefRed/settings/initialize_settings.py
+++ RefRed/settings/initialize_settings.py
@@ -27,7 +27,6 @@ class InitializeSettings(object):
                 if _value in ['', 'None']:
                     _value = o_list_settings.__dict__[_key]
                 else:
-                    print("THIS IS THE KEY :: " + _key)
                     _value = float(_value)
             _gui_metadata[_key] = _value
 
