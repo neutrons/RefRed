1c4355991fe288437b2bd83b9587e3bb494e55b5
Pete Peterson <petersonpf@ornl.gov>
2021-10-22 16:49:34 -0400

    created __eq__ method to simplify tests

--- RefRed/settings/list_settings.py
+++ RefRed/settings/list_settings.py
@@ -9,3 +9,26 @@ class ListSettings(object):
         self.clocking_pixel = [121, 197]
         self.angle_offset = 0.016
         self.angle_offset_error = 0.001
+
+    def __eq__(self, other) -> bool:
+        if isinstance(other, dict):
+            if self.q_min != other["q_min"]:
+                return False
+            if self.d_q0 != other["d_q0"]:
+                return False
+            if self.dq_over_q != other["dq_over_q"]:
+                return False
+            if self.tof_bin != other["tof_bin"]:
+                return False
+            if self.q_bin != other["q_bin"]:
+                return False
+            if self.clocking_pixel != other["clocking_pixel"]:
+                return False
+            if self.angle_offset != other["angle_offset"]:
+                return False
+            if self.angle_offset_error != other["angle_offset_error"]:
+                return False
+
+            return True
+        # don't know how to compare so must be different
+        return False
--- test/unit/RefRed/settings/test_initialize_settings.py
+++ test/unit/RefRed/settings/test_initialize_settings.py
@@ -1,5 +1,6 @@
 # package imports
 from RefRed.settings.initialize_settings import InitializeSettings
+from RefRed.settings.list_settings import ListSettings
 
 # third party packages
 import pytest
@@ -11,34 +12,29 @@ class TestInitializeSettings(object):
 
     def test_initialize_default_settings(self):
         InitializeSettings(self)
-        assert len(self.gui_metadata) > 0
-        assert self.gui_metadata['q_min'] == 0.005
-        assert self.gui_metadata['d_q0'] == 0.0004
-        assert self.gui_metadata['dq_over_q'] == 0.005
-        assert self.gui_metadata['tof_bin'] == 40  # micros
-        assert self.gui_metadata['q_bin'] == 0.01  # logarithmic binning
-        assert self.gui_metadata['clocking_pixel'] == [121, 197]
-        assert self.gui_metadata['angle_offset'] == 0.016
-        assert self.gui_metadata['angle_offset_error'] == 0.001
+        assert ListSettings() == self.gui_metadata
 
     @mock.patch("qtpy.QtCore.QSettings.value")
     def test_initialize_qsettings(self, mockValueMethod):
-        values = {'q_min': 1.005,
-                  'd_q0': 1.0004,
-                  'dq_over_q': 1.005,
-                  'tof_bin': 41,
-                  'q_bin': 1.01,
-                  'clocking_pixel': '1121, 1197',
-                  'angle_offset': 1.016,
-                  'angle_offset_error': 1.001}
+        values = {
+            'q_min': 1.005,
+            'd_q0': 1.0004,
+            'dq_over_q': 1.005,
+            'tof_bin': 41,
+            'q_bin': 1.01,
+            'clocking_pixel': '1121, 1197',
+            'angle_offset': 1.016,
+            'angle_offset_error': 1.001,
+        }
 
         def side_effect(arg):
             return values[arg]
+
         mockValueMethod.side_effect = side_effect
         InitializeSettings(self)
         mockValueMethod.assert_has_calls([mock.call(k) for k in values.keys()])
         for k in values.keys():
-            if k != 'clocking_pixel':
+            if k != "clocking_pixel":
                 assert self.gui_metadata[k] == values[k]
             else:
                 assert self.gui_metadata[k] == [1121, 1197]
