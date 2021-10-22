96e8a3f6df2ab381f463adc7d50eabcc7b95c57d
Chen Zhang <zhangc@ornl.gov>
2021-10-22 11:35:49 -0400

    1st round refactor of explot_plot_ascii
    remove unused import

--- RefRed/export/export_plot_ascii.py
+++ RefRed/export/export_plot_ascii.py
@@ -1,6 +1,5 @@
 import os
 from pathlib import Path
-from typing import List
 import RefRed.utilities
 from RefRed.export.output_reduced_data import OutputReducedData
 from RefRed.gui_handling.gui_utility import GuiUtility
@@ -16,6 +15,27 @@ class ExportPlotAscii:
     def __init__(self, parent=None, data_type: str = "yt"):
         self.parent = parent
         self.data_type = data_type
+
+    def get_default_filename(self, run_number: int, datatype: str) -> str:
+        """Return a default filename based on run number and data/plot type"""
+        # NOTE:
+        # figure layout as of 10-22-2021
+        # ------------  main window ------------|
+        # |     yt_plot      |     yi_plot      |
+        # |------------------|------------------|
+        # |     it_plot      |     ix_plot      |
+        # |-------------------------------------|
+        # |                table                |
+        # |-------------------------------------|
+        basepath = Path(self.parent.path_ascii)
+        suffix = {
+            "yt": "2dPxVsTof",
+            "ix": "ix",
+            "it": "yt",
+            "yi": "rpx",
+        }[datatype]
+        filename = f"REFL_{run_number}_{suffix}.txt"
+        return str(basepath / filename)
 
     def export(self):
         _data_type = self.data_type
@@ -33,100 +53,118 @@ class ExportPlotAscii:
             raise ValueError(f"data type {_data_type} not supported.")
 
     def export_yt(self):
-        parent = self.parent
-        big_table_data = parent.big_table_data
-        [row, col] = self.get_current_row_col_displayed()
-        _data = big_table_data[row, col]
-        default_filename = f"REFL_{_data.run_number}_2dPxVsTof.txt"
-        path = Path(parent.path_ascii)
-        default_filename = path / default_filename
+        # grab the active data
+        active_data = self.get_active_data()
+        # generate default filename for user
+        default_filename = self.get_default_filename(
+            active_data.run_number,
+            self.data_type,
+        )
+        # ask user
         caption = "Create 2D Pixel VS TOF"
-        filename, _ = getSaveFileName(parent, caption, str(default_filename))
-
+        filename, _ = getSaveFileName(self.parent, caption, default_filename)
+        # save data ONLY when filename is valid
         if filename.strip():
-            # valid filename, save the data
-            parent.path_ascii = os.path.dirname(filename)
+            # update cached dir
+            self.parent.path_ascii = os.path.dirname(filename)
+            # sanity check on extension
             filename = makeSureFileHasExtension(filename, default_ext=".txt")
-            image = _data.ytofdata
-            RefRed.utilities.output_2d_ascii_file(filename, image)
+            # get data to save
+            data_to_save = active_data.ytofdata
+            # save to file
+            RefRed.utilities.output_2d_ascii_file(filename, data_to_save)
 
     def export_ix(self):
-        parent = self.parent
-        big_table_data = parent.big_table_data
-        [row, col] = self.get_current_row_col_displayed()
-        _data = big_table_data[row, col]
-        default_filename = f"REFL_{_data.run_number}_ix.txt"
-        path = Path(parent.path_ascii)
-        default_filename = path / default_filename
+        # grab the activate data
+        active_data = self.get_active_data()
+        # generate the default filename for user
+        default_filename = self.get_default_filename(
+            active_data.run_number,
+            self.data_type,
+        )
+        # ask user input
         caption = "Create Counts vs Pixel (low resolution range) ASCII File"
-        filename, _ = getSaveFileName(parent, caption, str(default_filename))
-
+        filename, _ = getSaveFileName(self.parent, caption, default_filename)
+        # save ONLY when given filename is valid
         if filename.strip():
-            # filename valid, save the data
-            parent.path_ascii = os.path.dirname(filename)
+            # update parent cached dir
+            self.parent.path_ascii = os.path.dirname(filename)
+            # sanitizing filename extension
             filename = makeSureFileHasExtension(filename, default_ext=".txt")
-            countsxdata = _data.countsxdata
-            pixelaxis = list(range(len(countsxdata)))
-
+            # get data/str to save
+            countsx = active_data.countsxdata
+            pixelaxis = list(range(len(countsx)))
             text = ["#Counts vs Pixels (low resolution range)", "#Pixel - Counts"]
-            text += [f"{p} {c}" for p, c in zip(pixelaxis, countsxdata)]
-
+            text += [f"{p} {c}" for p, c in zip(pixelaxis, countsx)]
+            # save to file
             RefRed.utilities.write_ascii_file(filename, text)
 
     def export_it(self):
-        parent = self.parent
-        big_table_data = parent.big_table_data
-        [row, col] = self.get_current_row_col_displayed()
-        _data = big_table_data[row, col]
-        default_filename = f"REFL_{_data.run_number}_yt.txt"
-        path = Path(parent.path_ascii)
-        default_filename = path / default_filename
+        # grab the active data
+        active_data = self.get_active_data()
+        # generate the default filename for user
+        default_filename = self.get_default_filename(
+            active_data.run_number,
+            self.data_type,
+        )
+        # ask user for input
         caption = "Create Counts vs TOF ASCII File"
-        filename, _ = getSaveFileName(parent, caption, str(default_filename))
-
+        filename, _ = getSaveFileName(self.parent, caption, default_filename)
+        # save ONLY when given filename is valid
         if filename.strip():
-            # valid filename, save now
-            parent.path_ascii = os.path.dirname(filename)
-            countstofdata = _data.countstofdata
+            # update parent cached dir
+            self.parent.path_ascii = os.path.dirname(filename)
+            # sanitizing filename extension
             filename = makeSureFileHasExtension(filename, default_ext=".txt")
-            tof = _data.tof_axis_auto_with_margin
-            if tof[-1] > 1000:
-                tof /= 1000.0
-
+            # get data/str to save
+            countstof = active_data.countstofdata
+            tof = active_data.tof_axis_auto_with_margin
+            scale = 1000.0 if tof[-1] > 1000 else 1.0
+            tof /= scale
             text = ["#Counts vs  TOF", "#TOF(ms) - Counts"]
-            text += [f"{t} {c}" for t, c in zip(tof[:-1], countstofdata)]
+            text += [f"{t} {c}" for t, c in zip(tof[:-1], countstof)]
             text.append(str(tof[-1]))
+            # save to file
             RefRed.utilities.write_ascii_file(filename, text)
 
     def export_yi(self):
-        parent = self.parent
-        big_table_data = parent.big_table_data
-        [row, col] = self.get_current_row_col_displayed()
-        _data = big_table_data[row, col]
-        default_filename = f"REFL_{_data.run_number}_rpx.txt"
-        path = Path(parent.path_ascii)
-        default_filename = path / default_filename
+        # grab the active data
+        active_data = self.get_active_data()
+        # generate the default filename for user
+        default_filename = self.get_default_filename(
+            active_data.run_number,
+            self.data_type,
+        )
+        # ask user for input
         caption = "Create Counts vs Pixel ASCII File"
-        filename, _ = getSaveFileName(parent, caption, str(default_filename))
-
+        filename, _ = getSaveFileName(self.parent, caption, default_filename)
+        # save ONLY when given filename is valid
         if filename.strip():
-            # valid filename, save now
-            parent.path_ascii = os.path.dirname(filename)
+            # update parent cached dir
+            self.parent.path_ascii = os.path.dirname(filename)
+            # sanitizing filename extension
             filename = makeSureFileHasExtension(filename, default_ext=".txt")
-            ycountsdata = _data.ycountsdata
-            pixelaxis = list(range(len(ycountsdata)))
-
+            # get data/str to save
+            ycounts = active_data.ycountsdata
+            pixelaxis = list(range(len(ycounts)))
             text = ["#Counts vs Pixels", "#Pixel - Counts"]
-            text += [f"{p} {c}" for p, c in zip(pixelaxis, ycountsdata)]
-
+            text += [f"{p} {c}" for p, c in zip(pixelaxis, ycounts)]
+            # save to file
             RefRed.utilities.write_ascii_file(filename, text)
 
+    # -------------------------------------------------------------------- #
+    # NOTE:                                                                #
+    # this module cannot ensure that the following functions will always   #
+    # behave exactly as they were intended due to the external dependency  #
+    # nature.                                                              #
+    # -------------------------------------------------------------------- #
     def export_stitched(self):
         _tmp = OutputReducedData(parent=self.parent)
         _tmp.show()
 
-    def get_current_row_col_displayed(self) -> List[int]:
+    def get_active_data(self):
+        """Find the active/selected data from GUI"""
         o_gui_utility = GuiUtility(parent=self.parent)
         row = o_gui_utility.get_current_table_reduction_check_box_checked()
         col = o_gui_utility.get_data_norm_tab_selected()
-        return [row, col]
+        return self.parent.big_table_data[row, col]
