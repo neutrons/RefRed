491749b17e049cded408e1cea82523f41b06a99e
Chen Zhang <zhangc@ornl.gov>
2021-10-26 16:06:16 -0400

    refactor and unit test layout for OutputReducedData

---
+++ test/unit/RefRed/export/test_output_reduced_data.py
@@ -0,0 +1,111 @@
+# package imports
+
+# 3rd party imports
+import pytest
+
+
+class TestOutputReducedData:
+    def setup_class(self):
+        # mock a parent
+        # instantiate the class
+        pass
+
+    def teardown_class(self):
+        print(f"Test tear down for {self}")
+
+    def test_auto_qmin_button_clicked(self):
+        pass
+
+    def test_output_format_radio_buttons_event(self):
+        pass
+
+    def test_create_reduce_ascii_button_event(self):
+        pass
+
+    def test_create_n_files(self):
+        pass
+
+    def test_create_1_common_file(self):
+        pass
+
+    def test_save_back_widget_parameters_used(self):
+        pass
+
+    def test_is_folder_access_granted(self):
+        pass
+
+    def test_write_n_ascii(self):
+        pass
+
+    def test_format_n_filename(self):
+        pass
+
+    def test_write_1_common_ascii(self):
+        pass
+
+    def test_generate_selected_sf(self):
+        pass
+
+    def test_apply_sf(self):
+        pass
+
+    def test_create_output_file(self):
+        pass
+
+    def test_format_metadata(self):
+        pass
+
+    def test_collect_list_wks(self):
+        pass
+
+    def test_get_q_range(self):
+        pass
+
+    def test_apply_scaling_factor(self):
+        pass
+
+    def test_retrieve_sf(self):
+        pass
+
+    def test_retrieve_individual_metadata(self):
+        pass
+
+    def test_retrieve_metadata(self):
+        pass
+
+    def test_retrieve_total_counts(self):
+        pass
+
+    def test_retrieve_pcCharge(self):
+        pass
+
+    def test_retrieve_scaling_factor(self):
+        pass
+
+    def test_create_file(self):
+        pass
+
+    def test_produce_data_without_common_q_axis(self):
+        pass
+
+    def test_produce_data_with_common_q_axis(self):
+        pass
+
+    def test_applySF(self):
+        pass
+
+    def test_format_data(self):
+        pass
+
+    def test_centering_q_axis(self):
+        pass
+
+    def test_cleanup_data(self):
+        pass
+
+    def test_closeEvent(self):
+        pass
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
