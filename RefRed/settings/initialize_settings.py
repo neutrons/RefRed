e28d42bbec87cfd5beb0db98e9e745e6ad41f220
Pete Peterson <petersonpf@ornl.gov>
2021-10-27 09:54:37 -0400

    Refactor code

--- RefRed/settings/initialize_settings.py
+++ RefRed/settings/initialize_settings.py
@@ -4,31 +4,35 @@ from RefRed.settings.list_settings import ListSettings
 
 
 class InitializeSettings(object):
-
-    list_key = []
-
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, qsettings=None):
         self.parent = parent
 
         o_list_settings = ListSettings()
-        _list_keys = list(o_list_settings.__dict__.keys())
 
-        _settings = QSettings(ORGANIZATION, APPNAME)
+        if qsettings:
+            _settings = qsettings
+        else:  # load from filesystem
+            _settings = QSettings(ORGANIZATION, APPNAME)
+
         _gui_metadata = {}
-        for _key in _list_keys:
-            if _key == 'clocking_pixel':
-                _value = str(_settings.value(_key) or '')
-                if _value == '':
-                    _value = o_list_settings.__dict__[_key]
-                else:
-                    [px1, px2] = _value.split(",")
-                    _value = [int(px1), int(px2)]
-            else:
-                _value = str(_settings.value(_key) or '')
-                if _value == '':
-                    _value = o_list_settings.__dict__[_key]
-                else:
-                    _value = float(_value)
-            _gui_metadata[_key] = _value
+        for _key in list(o_list_settings.__dict__.keys()):
+            _gui_metadata[_key] = self.__getValue(_key, o_list_settings, _settings)
 
         self.parent.gui_metadata = _gui_metadata
+
+    def __getValue(self, key: str, lsettings, qsettings):
+        # default value
+        value = lsettings.__dict__[key]
+        # value from qsettings
+        if qsettings.contains(key):
+            _value = str(qsettings.value(key) or '').strip()
+            if _value:
+                value = _value
+
+            # convert to correct primative type
+            if key == 'clocking_pixel':  # split into two values
+                value = [int(val) for val in value.split(',')]
+            else:
+                value = float(value)
+
+        return value
