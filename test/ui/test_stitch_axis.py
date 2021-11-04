f3d5ed3418e9cd61b42b1f4638451010eab8bfa6
Chen Zhang <zhangc@ornl.gov>
2021-11-04 08:36:46 -0400

    switch fixture as suggested

--- test/ui/test_stitch_axis.py
+++ test/ui/test_stitch_axis.py
@@ -1,50 +1,30 @@
-import os
 import pytest
-from RefRed.main import MainGui
 from qtpy import QtCore, QtWidgets
 
 
 wait = 100
 
 
-def test_stitch_plot(qtbot, data_server):
+def test_stitch_plot(qtbot, main_gui, data_server):
     """Test the axis adjustment in stitching plot"""
-    # Create the main window.
-    main = MainGui()
-    qtbot.addWidget(main)
-    main.show()  # Only for human inspection. This line should be commented once the test passes.
-
-    # Load data via xml file
-    xml_file = data_server.path_to("REF_L_188299_to_188301.xml")
-    main.path_config = os.path.dirname(xml_file)
-
-    def qfiledialog_handler():
-        def handler():
-            dialog = main.findChild(QtWidgets.QFileDialog)
-            line_edit = dialog.findChild(QtWidgets.QLineEdit)
-            qtbot.keyClicks(line_edit, os.path.basename(xml_file))
-            qtbot.wait(wait)
-            qtbot.keyClick(line_edit, QtCore.Qt.Key_Enter)
-
-        QtCore.QTimer.singleShot(5 * wait, handler)  # wait for `lapse` time, then execute `handler`
-
-    qfiledialog_handler()
-    main.load_configuration()
-    qtbot.wait(wait)
+    main_window = main_gui(
+        configuration=data_server.path_to("REF_L_188299_to_188301.xml"),
+        show=True,
+    )
 
     # Run reduce
-    main.run_reduction_button()
+    main_window.run_reduction_button()
     qtbot.wait(wait)
-    data_stitching_Tab = main.findChild(QtWidgets.QWidget, "data_stitching_Tab")
+    data_stitching_Tab = main_window.findChild(QtWidgets.QWidget, "data_stitching_Tab")
     qtbot.mouseClick(data_stitching_Tab, QtCore.Qt.LeftButton)
     qtbot.wait(wait)
 
     #
-    stitch_plot = main.findChild(QtWidgets.QWidget, "data_stitching_plot")
+    stitch_plot = main_window.findChild(QtWidgets.QWidget, "data_stitching_plot")
     # mimic right click on the x-axis
     stitch_plot._singleClick(True, True, 0, 0)
     qtbot.wait(wait)
-    popup = main.findChild(QtWidgets.QMainWindow, "ManualXAxisControl")
+    popup = main_window.findChild(QtWidgets.QMainWindow, "ManualXAxisControl")
     # - adjust xmin
     xmin_line_edit = popup.findChild(QtWidgets.QLineEdit, "x_min_value")
     xmin_line_edit.setText("0.01")
@@ -63,7 +43,7 @@ def test_stitch_plot(qtbot, data_server):
     # mimic right click on the y-axis
     stitch_plot._singleClick(True, False, 0, 0)
     qtbot.wait(wait)
-    popup = main.findChild(QtWidgets.QMainWindow, "ManualYAxisControl")
+    popup = main_window.findChild(QtWidgets.QMainWindow, "ManualYAxisControl")
     assert popup
     # - adjust ymin
     popup.ui.y_min_value.setText("0.3")
