588b169e04f35e45ae7511f6707363708e947532
Ross Whitfield <whitfieldre@ornl.gov>
2021-11-23 10:50:00 -0500

    Cleanup start_refred.py

--- scripts/start_refred.py
+++ scripts/start_refred.py
@@ -7,52 +7,12 @@ import os
 import sys
 
 # import mantid once before qt, this fixes some issues with running in production
-import mantid
+import mantid  # noqa: F401
 
 # must be imported through qtpy before any other gui imports
 from qtpy.QtWidgets import QApplication, QSplashScreen
 from qtpy.QtGui import QPixmap
 from qtpy.QtCore import Qt
-
-
-def mantid_location(valid_versions, root_location='/opt'):
-    r"""
-    Find the location of mantid's installation. Locations under /opt are preferred over local installs.
-    :param List[str] valid_versions: mantid versions. each with format 'Major.Minor.Patch', e.g. '6.0.0'
-    :return str: absolute path to mantid installation
-    :except ImportError: a valid version of Mantid is not found
-    """
-
-    mantid_path = None  # value to return by this function
-
-    # Check for installed Mantid versions under /opt
-    short_version = {'6.1.0': '61', '6.0.0': '60', '5.1.0': '51'}
-    for _version in valid_versions:
-        install_path = os.path.join(root_location, 'mantid' + short_version[_version])  # e.g "/opt/mantid51"
-        if os.path.isdir(install_path):  # check only for the existence of the directory, but not its contents
-            mantid_path = install_path
-            break
-
-    # Check location in the PYTHONPATH
-    if not mantid_path:
-        try:
-            import mantid
-
-            mantid_version = mantid.__version__
-        except ImportError:
-            raise ImportError('Could not find a version of Mantid')
-        if mantid_version not in valid_versions:
-            raise ImportError('Mantid version is not one of ' + str(valid_versions))
-        mantid_path = os.path.dirname(mantid.__file__)
-
-    return mantid_path
-
-
-# TODO fetch the valid versions from a configuration file
-# mantid_path = mantid_location(['5.1.0'], root_location='/opt')
-# sys.path.insert(0, os.path.join(mantid_path, 'bin'))
-# sys.path.insert(1, os.path.join(mantid_path, 'lib'))
-
 
 # if script was run from commandline
 try:
