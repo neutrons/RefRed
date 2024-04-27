938c81bb3ef7e747a1b6fcd8343a8fb01e6c27fb
Jose Borreguero <borreguero@gmail.com>
2024-04-27 14:00:18 -0400

    test for file not in the data server
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>

--- test/test_fixtures.py
+++ test/test_fixtures.py
@@ -7,6 +7,8 @@ from pathlib import Path
 
 def test_data_server(data_server):
     assert Path(data_server.path_to('easy_data_set.csv')).exists()
+    with pytest.raises(FileNotFoundError):
+        Path(data_server.path_to('impossibe_to_find.dat')).exists()
 
 
 if __name__ == '__main__':
