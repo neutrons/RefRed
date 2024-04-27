10162e969d16b0bb184a80fe8af57c0d5a105a18
Jose Borreguero <borreguero@gmail.com>
2024-04-27 13:07:23 -0400

    deadtime  unit tests for SFCalculator.apply_deadtime_update
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>

--- test/unit/RefRed/sf_calculator/test_sf_calculator.py
+++ test/unit/RefRed/sf_calculator/test_sf_calculator.py
@@ -1,12 +1,34 @@
+# third party imports
 import pytest
+from qtpy.QtCore import Qt
+
+# RefRed imports
+from RefRed.sf_calculator.sf_calculator import SFCalculator
 
 
-def test_sf_calculator():
-    """Test class SFCalculator
+class TestSFCalculator:
+    @pytest.fixture(autouse=True)
+    def setup_class(self, qtbot):
+        self.app = SFCalculator()
+        qtbot.addWidget(self.app)
 
-    Note: this is a pytest-pyqt test
-    """
-    return
+    def test_apply_deadtime_update_checked(self, qtbot):
+        """Test the apply_deadtime_update function when the checkbox is checked."""
+        # Ensure checkbox starts unchecked
+        self.app.ui.deadtime_entry.applyCheckBox.setChecked(False)
+        # Simulate checking the checkbox
+        qtbot.mouseClick(self.app.ui.deadtime_entry.applyCheckBox, Qt.LeftButton)
+        # Assert that the apply_deadtime attribute is now True
+        assert self.app.apply_deadtime is True
+
+    def test_apply_deadtime_update_unchecked(self, qtbot):
+        """Test the apply_deadtime_update function when the checkbox is unchecked."""
+        # Ensure checkbox starts checked
+        self.app.ui.deadtime_entry.applyCheckBox.setChecked(True)
+        # Simulate unchecking the checkbox
+        qtbot.mouseClick(self.app.ui.deadtime_entry.applyCheckBox, Qt.LeftButton)
+        # Assert that the apply_deadtime attribute is now False
+        assert self.app.apply_deadtime is False
 
 
 if __name__ == '__main__':
